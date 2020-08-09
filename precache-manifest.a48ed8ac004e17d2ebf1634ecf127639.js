c64432b9a533fa14cb0799487657e924cd7e4f08
Sean <seanho96@yahoo.com>
2020-08-09 17:22:30 +0800

    Updates

---
+++ precache-manifest.a48ed8ac004e17d2ebf1634ecf127639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1b10f4926b09f5a150ef2539ba420b",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "c36fe60202fc53bf41e5",
+    "url": "/spotify-clone/static/css/main.c8e2e44e.chunk.css"
+  },
+  {
+    "revision": "e72756f8b7504439f46e",
+    "url": "/spotify-clone/static/js/2.5e4d114a.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/spotify-clone/static/js/2.5e4d114a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36fe60202fc53bf41e5",
+    "url": "/spotify-clone/static/js/main.46647cdd.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
